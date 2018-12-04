45ec4063d2201093822c7e59bbf1b188a7895529
Brandon Woolard <brandon.woolard.2019@student.lhschools.org>
2018-12-04 10:02:52 -0500

    Created Strings creator

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,17 @@ public class Main {
     public static void main(String[]args){
         Scanner usersInput = new Scanner(System.in);
         String usersResponseWord = "";
+        String item1 = "";
+        String item2 = "";
+        String item3 = "";
+        String item4 = "";
+        String idk = "";
         int usersResponseNumber = 0;
         int a = 0;
         int r = 0;
+        int u = 0;
+        int y = 0;
+        int t = 0;
         boolean Loop = true;
         while(Loop) {
             if(a>0){
@@ -16,28 +24,65 @@ public class Main {
             usersResponseNumber = Integer.parseInt(usersInput.nextLine());
             a = usersResponseNumber;
             r = usersResponseNumber;
-            if(a>0) {
+            /*if(a>0) {
                 System.out.println("What's the name of Item 1?");
                 usersResponseWord = usersInput.nextLine();
-                createStrings(usersResponseWord,r);
+                System.out.println(createStrings(usersResponseWord,r,u));
+                System.out.println("What's the name of Item 2?");
+                usersResponseWord = usersInput.nextLine();
+                System.out.println("What's the name of Item 3?");
+                usersResponseWord = usersInput.nextLine();
+                System.out.println("What's the name of Item 4?");
+                usersResponseWord = usersInput.nextLine();
+                System.out.println(createStrings(usersResponseWord,r,u));
+                System.out.println("R: "+r);
+                System.out.println("U: "+u);
+                System.out.println(printItems(usersResponseWord,item1,item2,r,u));
+            }*/
+            boolean createItems = true;
+            while(createItems){
+                y++;
+                if(y>1){
+                    r--;
+                }
+                System.out.println("What's the name of Item "+y+"?");
+                usersResponseWord = usersInput.nextLine();
+                System.out.println(createStrings(usersResponseWord,usersResponseNumber,r));
+                System.out.println("What's the quantity of "+createStrings(usersResponseWord,usersResponseNumber,r)+" did you purchase?");
+                System.out.println("R:"+r);
+                System.out.println("Y:"+y);
+                if(r==0){
+                    createItems = false;
+                }
             }
+            System.out.println("Out of Loop!");
         }
     }
     /*public static String suppliesBought(String usersResponseWord){
 
     }*/
-    public static String createStrings(String usersResponseWord, int r){
-        if(r>0){
-            r--;
-        }
+    public static String createStrings(String usersResponseWord, int usersResponseNumber, int r){
         String idk = "";
-        String item1 = usersResponseWord;
-        String item2 = usersResponseWord;
-        String item3 = usersResponseWord;
-        String item4 = usersResponseWord;
-        String item5 = usersResponseWord;
+        if(r==4) {
+            String item4 = usersResponseWord;
+            idk = item4;
+            int item4Cost = usersResponseNumber;
+        }
+        else if(r==3) {
+            String item3 = usersResponseWord;
+            idk = item3;
+        }
+        else if(r==2) {
+            String item2 = usersResponseWord;
+            idk = item2;
+        }
+        else if(r==1) {
+            String item1 = usersResponseWord;
+            idk = item1;
+        }
+        /*String item5 = usersResponseWord;
         String item6 = usersResponseWord;
-        String item7 = usersResponseWord;
+        String item7 = usersResponseWord;*/
         return idk;
     }
 }
